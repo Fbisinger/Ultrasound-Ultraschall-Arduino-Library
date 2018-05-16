01261d26e1e12bb20ae49c7f2f267041882b1202
Fbisinger <37881821+Fbisinger@users.noreply.github.com>
2018-05-16 21:01:32 +0200

    Add files via upload

---
+++ Ultraschall_Demo/Ultraschall_Demo.ino
@@ -0,0 +1,20 @@
+/* ============================================
+Arduino Ultraschall - Sensor Bibliothek
+    2018 F. Bisinger
+Diese Bibliothek vereinfacht das Auslesen eines
+Ultraschallsensors.
+===============================================
+*/
+
+#include "Ultraschall.h"
+Ultraschall Mein_Sensor; /*an Stelle von "Sensor_Demo" kann beliebiger Name stehen (dort kann der
+                            Sensor benannt werden, dies muss jedoch auch im .getEntfernung - Befehl beachtet werden)*/
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.print(Mein_Sensor.getEntfernung(3,4));/* Pins nach folgendem Schema: xyz.getEntfernung(triggerPin,echoPin)*/
+  Serial.println(" cm");
+  delay(100);
+}
